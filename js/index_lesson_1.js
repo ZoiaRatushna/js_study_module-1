bb1458902fdc6070103d7dde9db321b732abfd6f
ZoiaRatushna <93883758+ZoiaRatushna@users.noreply.github.com>
2022-02-10 13:38:59 +0200

    js_lesson

---
+++ js/index_lesson_1.js
@@ -0,0 +1,97 @@
+// let и const
+
+let x = 6;
+
+x = 7;
+
+const y = 8;
+
+// console.log(x);
+
+// string, number, boolean, null, undefined
+
+let empty;
+
+// console.log(empty);
+
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+var oldVar = 8;
+
+let str = "HHHH 8";
+
+str.toLowerCase();
+
+// console.log(str.toLowerCase());
+
+// console.log(str.length);
+
+let num = 10;
+
+// console.log(num % 3);
+
+// false | null | undefined | "" | 0 | NaN - falsy values
+
+// операторы сравнения
+// > < >= <= == === != !==
+
+// console.log(5 > 4);
+
+// console.log(10 >= "7");
+
+// console.log("2" > "12");
+
+// console.log("2" < "12");
+
+// console.log("4" == 4);
+
+// console.log("6" === 6);
+
+// console.log("false" === false);
+
+// console.log(1 == true);
+
+// console.log(1 === true);
+
+// console.log("0" == false);
+
+// console.log("0" === false);
+
+// console.log("Papaya" < "papaya");
+
+// console.log("Papaya" === "papaya");
+
+// console.log(undefined == null);
+
+// console.log(undefined === null);
+
+// Логичесие операторы
+
+// && || !
+
+// console.log(true && 3);
+
+// console.log(false && 3);
+
+// console.log(true && 4 && "kiwi");
+
+// console.log(true && 0 && "kiwi");
+
+// console.log(true || 3);
+
+// console.log(true || 3 || 4);
+
+// console.log(true || false || 7);
+
+// console.log(null || 2 || undefined);
+
+// console.log((1 && null && 2) > 0);
+
+// console.log(null || (2 && 3) || 4);
+
+let t = 8 + 10;
+
+t = 900;
+
+console.log(Number.isNaN(8));
